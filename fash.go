727390bffbbc0a8a5329ac514be657e7c2bc0c91
Jim Kalafut <jim@kalafut.net>
2014-12-30 22:50:41 -0800

    Added channels

--- fash.go
+++ fash.go
@@ -1,34 +1,73 @@
 package main
 
 import (
+	"fmt"
 	"hash/fnv"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
 const SAMPLE_SIZE = 4096
+const WORKERS = 10
 
-func g(path string) uint64 {
+var wg, wg2 sync.WaitGroup
+
+var EXT = map[string]bool{
+	".jpg": true,
+	".mp4": true,
+}
+
+type File struct {
+	Path string
+	Size int64
+	Hash uint64
+}
+
+func g(files <-chan File, results chan<- File) {
 	buffer := make([]byte, SAMPLE_SIZE)
 	h := fnv.New64a()
+	for file := range files {
+		h.Reset()
+		f, _ := os.Open(file.Path)
+		f.Read(buffer)
+		h.Write(buffer)
 
-	f, _ := os.Open(path)
-	f.Read(buffer)
-	h.Write(buffer)
-
-	return h.Sum64()
+		file.Hash = h.Sum64()
+		results <- file
+	}
+	wg.Done()
 }
 
-func main() {
-	g("test2")
-	traverse("d://tmp//t7")
-}
-
-func traverse(root string) {
+func traverse(root string, files chan<- File) {
 	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		go g(path)
-		if filepath.Ext(path) == ".c" {
+		if true || EXT[filepath.Ext(path)] {
+			files <- File{Path: path, Size: info.Size()}
 		}
 		return nil
 	})
+	close(files)
+}
+
+func main() {
+	files := make(chan File, 100)
+	results := make(chan File, 100)
+
+	wg2.Add(1)
+	go func() {
+		for file := range results {
+			fmt.Printf("%s %d\n", file.Path, file.Hash)
+		}
+		wg2.Done()
+	}()
+
+	for w := 0; w < WORKERS; w++ {
+		wg.Add(1)
+		go g(files, results)
+	}
+	traverse(".", files)
+	wg.Wait()
+	close(results)
+	wg2.Wait()
+
 }
